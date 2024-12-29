d729a8bf35b4d80cdb37ddb4630e9f4c337b0951
josef.raz <josef.raz@brtech-it.eu>
2024-12-29 21:38:19 +0100

    Refactoring.

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// TODO return error in JSON: { "error": "..." }
 // Middleware for user authentication.
 //
 // If the user has a valid access token, it sets its ID in the context.
@@ -20,21 +21,30 @@ func Auth(c *gin.Context) {
 	userId, err := utils.TokenValid(utils.ExtractTokenFromContext(c), utils.GetSingleton().Config.AccessTokenSecret)
 	// Invalid token
 	if err != nil {
-		c.AbortWithStatus(http.StatusUnauthorized)
-		c.Error(err)
+		/*
+			c.AbortWithStatus(http.StatusUnauthorized)
+			c.Error(err)
+		*/
+		c.AbortWithError(http.StatusUnauthorized, err)
 		return
 	}
 	// Check if user exists
 	exists, err := database.UserExistsById(userId)
 	if err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		c.Error(err)
+		/*
+			c.AbortWithStatus(http.StatusInternalServerError)
+			c.Error(err)
+		*/
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	// User does not exist.
 	if !exists {
-		c.AbortWithStatus(http.StatusUnauthorized)
-		c.Error(errors.New("user does not exist"))
+		/*
+			c.AbortWithStatus(http.StatusUnauthorized)
+			c.Error(errors.New("user does not exist"))
+		*/
+		c.AbortWithError(http.StatusUnauthorized, errors.New("user does not exist"))
 		return
 	}
 	// Token is valid, set it to context and continue
